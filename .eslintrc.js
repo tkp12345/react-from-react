a4d3d496ebf79dc89c120a6876c9c8400d3869a0
roberto <tkp1@hanmail.net>
2024-04-12 11:45:33 +0900

    [feat] : 린트 설정 추가

---
+++ .eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = require('./lib/base-config/eslint-react.base.config');
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    endOfLine: 'lf',
+    singleQuote: true,
+    semi: false,
+    useTabs: false,
+    tabWidth: 2,
+    trailingComma: 'all',
+    printWidth: 120,
+    arrowParens: 'always',
+}
